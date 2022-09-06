ce8e8a833358647416c46c75b8218b267bc32ad7
Boris Spasojevic <boris.spasojevic@oracle.com>
2022-09-06 18:04:04 +0200

    Simplify test.sh by making it bash only.

--- test/test.sh
+++ test/test.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
-/usr/bin/test "$(../Perun echo_foo)" == "foo"
-/usr/bin/test "$(../Perun echo_foo bar)" == "foo"
-/usr/bin/test "$(../Perun echo_bar_and_args)" == "bar"
-/usr/bin/test "$(../Perun echo_bar_and_args arg)" == "bar arg"
-/usr/bin/test "$(../Perun echo_bar_and_args arg1 arg2)" == "bar arg1 arg2"
-/usr/bin/test "$(../Perun echo_empty_or_first_arg)" == "empty"
-/usr/bin/test "$(../Perun echo_empty_or_first_arg foo)" == "foo"
-/usr/bin/test "$(../Perun testArgMap)" == "NOT_PROVIDED"
-/usr/bin/test "$(../Perun testArgMap -d foo)" == "foo"
-/usr/bin/test "$(../Perun testArgMap -d)" == " "
-/usr/bin/test "$(../Perun testArgMap -d -t)" == " "
-/usr/bin/test "$(../Perun testArgMap -t -d)" == " "
-/usr/bin/test "$(../Perun testArgs -d foo bar baz)" == "bar baz "
-/usr/bin/test "$(../Perun testArgs foo -d bar baz)" == "foo baz "
-/usr/bin/test "$(../Perun testArgs foo bar -d baz)" == "foo bar "
+test "$(../Perun echo_foo)" == "foo"
+test "$(../Perun echo_foo bar)" == "foo"
+test "$(../Perun echo_bar_and_args)" == "bar"
+test "$(../Perun echo_bar_and_args arg)" == "bar arg"
+test "$(../Perun echo_bar_and_args arg1 arg2)" == "bar arg1 arg2"
+test "$(../Perun echo_empty_or_first_arg)" == "empty"
+test "$(../Perun echo_empty_or_first_arg foo)" == "foo"
+test "$(../Perun testArgMap)" == "NOT_PROVIDED"
+test "$(../Perun testArgMap -d foo)" == "foo"
+test "$(../Perun testArgMap -d)" == " "
+test "$(../Perun testArgMap -d -t)" == " "
+test "$(../Perun testArgMap -t -d)" == " "
+test "$(../Perun testArgs -d foo bar baz)" == "bar baz "
+test "$(../Perun testArgs foo -d bar baz)" == "foo baz "
+test "$(../Perun testArgs foo bar -d baz)" == "foo bar "
 mkdir dir_without_perunfile
 (
 cd dir_without_perunfile
-# Intentionally fails cause no task was given
 EDITOR="cat"
-/usr/bin/test "! $(../../Perun <<< 'Y')"
-/usr/bin/test "$(head -n 1 Perunfile)" == '#!/bin/bash'
-/usr/bin/test "$(../../Perun _s test 'echo foo')"
-/usr/bin/test "$(../../Perun test)" == "foo"
+# Intentionally fails cause no task was given
+test "! $(../../Perun <<< 'Y')"
+test "$(head -n 1 Perunfile)" == '#!/bin/bash'
+test "$(../../Perun _s test 'echo foo')"
+test "$(../../Perun test)" == "foo"
 )
 rm -rf dir_without_perunfile
